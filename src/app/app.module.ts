b9a1f0eccce695e03e461e6eb07680f7bbb504bc
Diego Abanto <dabanto@belatrixsf.com>
2017-09-12 16:44:51 -0500

    CREACION DE COMPONENTES - APROXIMACION POR TEMPLATE Y DATA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { DataComponent } from './components/data/data.component';
+
+// Import new components
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule
