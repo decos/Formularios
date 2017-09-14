1f1957d79a339c0cf810de9b56a62694799e29da
Diego Abanto <dabanto@belatrixsf.com>
2017-09-14 14:43:15 -0500

    TEMPLATE: VALIDACIONES INDEPENDIENTES  Y CAMBIO DE ESTILO

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -4,14 +4,25 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+      .ng-invalid.ng-touched:not(form) {
+        border: 1px solid red;
+      }
+    `]
 })
 export class TemplateComponent {
-
+  /*
   usuario:Object = {
     nombre: "Fernando",
     apellido:  "Herrera",
     correo: ""
+  }
+  */
+
+  usuario:Object = {
+    nombre: null,
+    apellido:  null,
+    correo: null
   }
 
   constructor() { }
